87851564a0d54d024085a41b04a4fdfe6447d596
msymt <nam.nam580@gmail.com>
2021-08-13 23:42:59 +0900

    add gettting GPS(lat, lng) info function

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,6 +1,14 @@
+#include <TinyGPS++.h>
+#include <vector>
+
 // motor
 const int motorA[3] = {13, 4, 25};  // AIN1, AIN2, PWMA
 const int motorB[3] = {14, 27, 26}; // BIN1, BIN2, PWMB
+
+// GPS
+static const uint32_t GPSBaud = 9600;
+TinyGPSPlus gps;
+HardwareSerial ss(2);
 
 // チャンネル
 const int CHANNEL_A = 0;
@@ -10,6 +18,9 @@ const int CHANNEL_B = 1;
 const int LEDC_TIMER_BIT = 8;
 // 周波数
 const int LEDC_BASE_FREQ = 490;
+
+// 出発地点の緯度・経度を取得する
+std::vector<double> get_start_lat_lng();
 
 void setup() {
   // put your setup code here, to run once:
@@ -26,13 +37,36 @@ void setup() {
   ledcAttachPin(motorA[2], CHANNEL_A);
   ledcAttachPin(motorB[2], CHANNEL_B);
 
-  // 出発地点の緯度・経度を取得する
-  // 目的地の緯度・軽度を取得する
+  /**
+   * GPSの設定
+   **/
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+
+  Serial.println("GPS start!");
+  // // 出発地点の緯度・経度を取得する
+  // std::vector<double> latLng = get_start_lat_lng();
+  // Serial.println(latLng[0]);
+  // Serial.println(latLng[1]);
+  // // 目的地の緯度・軽度を取得する
 }
 
-// 出発地点の緯度・経度を取得する
-void set_start_lat_lng(){
-
+// TODO：GPSの緯度・軽度を取得するまで待機，したら返す
+// 緯度・経度を取得する
+std::vector<double> get_lat_lng(){
+  std::vector<double> latLng = {0.0 , 0.0};
+  while(ss.available() > 0){
+    char c = ss.read();
+    gps.encode(c);
+    if(gps.location.isUpdated()){
+      latLng[0] = gps.location.lat();
+      latLng[1] = gps.location.lng();
+      Serial.print("Lat=\t");   Serial.print(gps.location.lat(), 6);
+      Serial.print(" Lng=\t");   Serial.println(gps.location.lng(), 6);
+      break;
+    }
+  }
+  return latLng;
 }
 
 // 目的地の緯度・経度を取得する
@@ -80,5 +114,11 @@ void move_left(){
 
 // put your main code here, to run repeatedly:
 void loop() {
+  // move_straight();
 
+  // 出発地点の緯度・経度を取得する
+  std::vector<double> latLng = get_lat_lng();
+  Serial.println(latLng[0]);
+  Serial.println(latLng[1]);
+  // 目的地の緯度・軽度を取得する
 }
